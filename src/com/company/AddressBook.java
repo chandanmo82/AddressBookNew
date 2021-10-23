f7261dd967eed650d85d688f3dcd413f4126372e
Chandan <chandanmohanty11111>
2021-10-23 14:11:59 +0530

    REFACTORED:UC2 add new contact

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -1,9 +1,28 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class AddressBook {
 
-    ArrayList<ContactPerson> contactList = new ArrayList<ContactPerson>();
+public class AddressBook implements AddressBookIF{
+
+    ArrayList <ContactPerson> contactList = new ArrayList<ContactPerson>();
+
+    @Override
+    public void addContact(ContactPerson person) {
+        contactList.add(person);
+
+    }
+
+    @Override
+    public void displayContents() {
+
+        Iterator<ContactPerson> iterator = contactList.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+    }
+
+
 }
-
---
+++ src/com/company/AddressBookIF.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface AddressBookIF {
+    public void addContact(ContactPerson person);
+
+    public void displayContents();
+}
+
